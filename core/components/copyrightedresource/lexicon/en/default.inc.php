fa75d2cae531244df3d13102024e7c7afe4d9648
Shaun McCormick <shaun@modx.com>
2011-11-28 14:33:18 -0600

    Complete step 1

---
+++ core/components/copyrightedresource/lexicon/en/default.inc.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @var array $_lang
+ * @package copyrightedresource
+ */
+$_lang['copyrightedresource'] = 'Copyrighted Page';
+$_lang['copyrightedresource_create_here'] = 'Create a Copyrighted Page Here';
--- core/components/copyrightedresource/model/copyrightedresource/copyrightedresource.class.php
+++ core/components/copyrightedresource/model/copyrightedresource/copyrightedresource.class.php
@@ -2,4 +2,28 @@
 /**
  * @package copyrightedresource
  */
-class CopyrightedResource extends modResource {}
+class CopyrightedResource extends modResource {
+    public $showInContextMenu = true;
+    
+    function __construct(xPDO & $xpdo) {
+        parent :: __construct($xpdo);
+        $this->set('class_key','CopyrightedResource');
+    }
+
+    public static function getControllerPath(xPDO &$modx) {
+        return $modx->getOption('copyrightedresource.core_path',null,$modx->getOption('core_path').'components/copyrightedresource/').'controllers/';
+    }
+
+    public function getContextMenuText() {
+        $this->xpdo->lexicon->load('copyrightedresource:default');
+        return array(
+            'text_create' => $this->xpdo->lexicon('copyrightedresource'),
+            'text_create_here' => $this->xpdo->lexicon('copyrightedresource_create_here'),
+        );
+    }
+
+    public function getResourceTypeName() {
+        $this->xpdo->lexicon->load('copyrightedresource:default');
+        return $this->xpdo->lexicon('copyrightedresource');
+    }
+}
